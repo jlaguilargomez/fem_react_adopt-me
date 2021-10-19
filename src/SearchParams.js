5649eb24994999651daff31d251386005f6561b2
Jose Luis Aguilar <jlaguigom@gmail.com>
2021-10-19 15:29:12 +0200

    Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["brid", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+
+  const BREEDS = [];
 
   function updateLocation(e) {
     setLocation(e.target.value);
@@ -19,6 +25,35 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           ></input>
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value=""></option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option value=""></option>
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
