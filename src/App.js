82634f87779877cae14e9fb25562f09c0c6feab6
Jose Luis Aguilar <jlaguigom@gmail.com>
2021-10-20 12:06:30 +0200

    React Router Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details></Details>
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
