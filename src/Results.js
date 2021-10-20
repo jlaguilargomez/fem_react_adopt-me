73233ff981ab1694c21a04c0869e232e22755f95
Jose Luis Aguilar <jlaguigom@gmail.com>
2021-10-20 10:24:18 +0200

    Component composition

---
+++ src/Results.js
@@ -0,0 +1,32 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  console.log(2, pets);
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No pets found</h2>
+      ) : (
+        pets.map(
+          ({ animal, id, name, breed, images, city, state, description }) => (
+            <Pet
+              animal={animal}
+              key={id}
+              name={name}
+              breed={breed}
+              images={images}
+              location={`${city}, ${state}`}
+              description={description}
+              id={id}
+            />
+
+            // It´s easier to make the following simplification but please, do not do this:
+            // <Pet {...pet} key={pet.id} />
+          )
+        )
+      )}
+    </div>
+  );
+};
+
+export default Results;
